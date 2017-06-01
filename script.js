42d09e8c70a37e4a30827b39c330aac11f289332
manishtiwari98 <manishtiwari9700@gmail.com>
2017-06-01 19:48:13 +0530

    Normol mode without styling

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+function addMemo(){
+	var mc=document.getElementById('memo_container');
+    var divElement=document.createElement('div');
+    divElement.className='text_container';
+    var memoText=document.getElementById('memoText').value;
+    divElement.innerHTML="<div><input class='eBtn' type='button' value='Edit' onClick='edit(this);'> <input class='rmBtn' type='button' value='Remove' onClick='rem(this);'> <hr/> </div>";
+    
+    
+    divElement.innerHTML+=memoText;
+    mc.appendChild(divElement);
+
+}
+
+function edit(id){
+	var target=id.parentElement.parentElement;
+if(id.value=="Edit"){
+
+target.contentEditable='true';
+id.value="Save";
+}
+else if(id.value=='Save')
+{
+target.contentEditable='false';
+id.value="Edit";
+
+}
+}
+
+
+function rem(id){
+
+var x=id.parentElement.parentElement;
+x.parentNode.removeChild(x);
+}
